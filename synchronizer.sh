59e4d17c0c09b4db8115050e0a4577b4684706e0
beitaz <beitaz@icloud.com>
2018-02-22 20:07:43 +0800

    修改同步脚本

--- synchronizer.sh
+++ synchronizer.sh
@@ -12,8 +12,8 @@ echo "同步时间：${current_time}" >> $log_file
 echo "工作目录：${current_dir}" >> $log_file
 echo "当前分支：${current_branch}" >> $log_file
 echo "所有分支：`git remote -v`" >> $log_file
-echo -e "\ngit stash"
-`git stash` >> $log_file
+echo -e "\ngit stash" >> $log_file
+$(git stash) >> $log_file
 # git checkout $remote_default_branch >> $log_file
 # command=`git fetch upstream` >> $log_file
 # git merge upstream/$remote_default_branch
